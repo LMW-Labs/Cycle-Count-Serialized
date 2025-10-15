cbd68a517b3762998d62e3ac21ad7a73912644e7
Austin <awyrick@lmwlabs.faith>
2025-10-15 17:11:55 +0000

    initial commit

---
+++ barcode-reconciler-app/src/App.jsx
@@ -0,0 +1,12 @@
+import BarcodeReconciler from './BarcodeReconciler';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <BarcodeReconciler />
+    </div>
+  );
+}
+
+export default App;
